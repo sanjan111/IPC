7626efd1ea5091e94e40760154825c6aa173c1a2
Sanjan I S <sanjanis111@gmail.com>
2025-08-29 15:42:51 +0530

    feat: Indicator and seatbelt working successfully No conflict, interrupt working as intended.

--- Codes/Test.c
+++ Codes/Test.c
@@ -3,6 +3,7 @@
 #include "implement_indicator.h"
 #include "PLL.h"
 #include "timer.h"
+#include "led.h"
 
 #define OFF 					0
 #define ON  					1
@@ -10,16 +11,19 @@
 #define LEFT_INDICATOR			1
 #define RIGHT_INDICATOR			2
 #define HAZARD_INDICATOR		3
+#define SEATBELT_INDICATOR		1
 
 int main(void)
 {
 	int hazard_switch = 0;
-	int left_switch = 0;
-	int right_switch = 1;
+	int left_switch = 1;
+	int right_switch = 0;
+	int seatbelt_switch = 1;
 
     PLL_Init();
     Timer_Init();
     SPI_Init();
+	LED_Init();
 	while (1)
 	{
 		if (hazard_switch == ON)
@@ -28,6 +32,7 @@ int main(void)
 			Indicator(LEFT_INDICATOR);
 		if (hazard_switch == OFF && right_switch == ON)
 			Indicator(RIGHT_INDICATOR);
-		
+		if (seatbelt_switch == ON)
+			LED_Status(SEATBELT_INDICATOR);
 	}
 }
---
+++ Codes/led.c
@@ -0,0 +1,15 @@
+#include "LPC17xx.h"
+#include <stdint.h>
+
+void LED_Init(void)
+{
+    LPC_GPIO1->FIODIR = (1 << 29);
+}
+
+void LED_Status(uint8_t status)
+{
+    if (status == 1)
+        LPC_GPIO1->FIOSET = (0x1 << 29);
+    else if (status == 0)
+        LPC_GPIO1->FIOCLR = (0x1 << 29);
+}
